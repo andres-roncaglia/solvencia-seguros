7e6e67d69a782e18c61a1b6bf751f591a15521b4
GamboaNicolas <gamboa.nicolas2002@gmail.com>
2024-11-02 10:32:50 -0300

    Variables renombradas

--- TP_seguros_script.R
+++ TP_seguros_script.R
@@ -1,19 +1,21 @@
 library(readxl)
-options(scipen = 9999)
-cuantias_2023 <- read_excel("C:/Users/operador/Downloads/Trabajo Final 2024 Base de Datos .xlsx")
-View(Trabajo_Final_2024_Base_de_Datos_)
-
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
+options(scipen = 9999)
+cuantias_2023 <- read_excel("Datos/Datos.xlsx")
+View(Trabajo_Final_2024_Base_de_Datos_)
+
+
 
 cuantias_2023 |> 
   ggplot()+
-  aes(x = `CUANTIA AJUSTADA`)+
+  aes(x = Cuantia_ajustada)+
   geom_density()
   
-mean(cuantias_2023$`CUANTIA AJUSTADA`)
-
+mean(cuantias_2023$Cuantia_ajustada)
+cuantias_2023$
 # Posibles distribuciones 
 
 
