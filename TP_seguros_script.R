cd8b0cbcb5e32f8f78d71e4a31cf9bff0be39d94
GamboaNicolas <gamboa.nicolas2002@gmail.com>
2024-11-04 09:30:33 -0300

    Corrección de formato de la base. Revisar warnings()

--- TP_seguros_script.R
+++ TP_seguros_script.R
@@ -3,19 +3,28 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
-options(scipen = 9999)
-cuantias_2023 <- read_excel("Datos/Datos.xlsx")
-View(Trabajo_Final_2024_Base_de_Datos_)
+options(scipen = 999999999999999)
+cuantias_2023 <- read_excel("Datos/Datos.xlsx") |> 
+  mutate(Marginal = ifelse(Cuantia_ajustada < 1000000, T,F))
+View(cuantias_2023)
 
-
+class(cuantias_2023$Cuantia_ajustada)
 
 cuantias_2023 |> 
   ggplot()+
   aes(x = Cuantia_ajustada)+
   geom_density()
-  
+
+table(cuantias_2023$Cuantia_ajustada)
+
+cut_interval(x = cuantias_2023$Cuantia_ajustada[cuantias_2023$Marginal == T],
+             length = 5000)
+
+
+
+sum(Probs)
 mean(cuantias_2023$Cuantia_ajustada)
-cuantias_2023$
+
 # Posibles distribuciones 
 
 
