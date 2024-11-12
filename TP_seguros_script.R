6de45a5b9d9fe438a176106a72bb5131b30f5344
GamboaNicolas <gamboa.nicolas2002@gmail.com>
2024-11-12 12:00:56 -0300

    Pareto simulacion hecha

--- TP_seguros_script.R
+++ TP_seguros_script.R
@@ -3,17 +3,31 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
-options(scipen = 999999999999999)
+options(scipen = 999)
 cuantias_2023 <- read_excel("Datos/Datos.xlsx") |> 
-  mutate(Marginal = ifelse(Cuantia_ajustada < 1000000, T,F))
+  mutate(Marginal = ifelse(Cuantia_ajustada < 900000, T,F),
+         Cuantia_ajustada = Cuantia_ajustada/1000)
+
 View(cuantias_2023)
 
-class(cuantias_2023$Cuantia_ajustada)
+Registro <- tibble(
+  anio = c(2021,2022,2023),
+  polizas = c(24725, 25348 , 25615),
+  siniestros = c(3023, 3581, nrow(cuantias_2023))
+) |> 
+  mutate(SinXpoliza = siniestros/polizas)
+
+lambda <- Registro$SinXpoliza[3]
+
+
+
 
 cuantias_2023 |> 
   ggplot()+
   aes(x = Cuantia_ajustada)+
   geom_density()
+
+
 
 table(cuantias_2023$Cuantia_ajustada)
 
